4f8f1f70cdae2c00d6a9fe43edbe368d5e443e52
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-09 18:32:19 +0200

    add lists

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,19 @@ import Main from "./comps/Main";
 import Welcome from "./comps/Welcome";
 
 function App() {
+  const numbers = [1, 2, 3, 4, 5];
   return (
     <>
       <Navbar />
       <Main />
       <Welcome />
       <p>{2 + 2}</p>
-      {true && <p>True</p>}
+      <h3>Numbers:</h3>
+      {numbers.map((number) => (
+        <p key={number}>{number}</p>
+      ))}
       {false && <p>True</p>}
+      {true && <p>Show me</p>}
       <Footer />
     </>
   );
