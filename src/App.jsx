2e117b5cebd3e714ad600cf26211314fdd5533b9
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-10 16:41:00 +0200

    Update App component to display user props

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Aloha</h1>
+      <h1>Props</h1>
+      <User name="John" age={30} />
     </>
   );
 }
+
+const User = (props) => {
+  return (
+    <>
+      <h1>My name is {props.name}</h1>
+      <h2>My age is {props.age}</h2>
+    </>
+  );
+};
 
 export default App;
