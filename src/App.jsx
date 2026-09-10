264162dc10d28ceb76e3deb5c75bbd85fd1f347d
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-10 18:49:51 +0200

    Add click counter and update Button component to include icon

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import { TbAccessible } from "react-icons/tb";
 import { FaShoppingCart } from "react-icons/fa";
 import Button from "./comps/Button";
 
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <h1>Card</h1>
       <div>
         <FaShoppingCart />
         <Button />
+      </div>
+      <p>Button clicked {count} times</p>
+      <div>
+        <button
+          style={{ marginTop: "16px", width: "100px" }}
+          onClick={() => setCount(count + 1)}>
+          Click Me
+        </button>
       </div>
     </>
   );
--- src/comps/Button.jsx
+++ src/comps/Button.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { FaBeer } from "react-icons/fa";
 
 const Button = () => {
   return (
     <div>
-      <button onClick={() => console.log("Button clicked!")}>Click Me</button>
+      <button onClick={() => console.log("Button clicked!")}>
+        <FaBeer style={{ marginRight: "8px", color: "gold" }} /> Click Me
+      </button>
     </div>
   );
 };
