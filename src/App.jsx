8cb9ef1895b70bbc8f6d989fdc306697e7776301
Aleksandar Stanisic <ciandre23@gmail.com>
2026-09-11 14:30:21 +0200

    crud ui

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,61 @@ const App = () => {
     releaseYear: 2010,
   });
 
+  const [movies, setMovies] = React.useState([
+    { id: 1, title: "Inception", releaseYear: 2010 },
+    { id: 2, title: "The Dark Knight", releaseYear: 2008 },
+    { id: 3, title: "Interstellar", releaseYear: 2014 },
+  ]);
+
   return (
     <div>
       <h1>Hello, World!</h1>
       <p>Title: {movie.title}</p>
       <p>Release Year: {movie.releaseYear}</p>
-
       <button
         onClick={() =>
           setMovie({ ...movie, title: "The Matrix", releaseYear: 1999 })
         }>
         Change Movie
       </button>
+      {movies.map((m) => (
+        <div key={m.id}>
+          <p>Title: {m.title}</p>
+          <p>Release Year: {m.releaseYear}</p>
+          button
+          <button
+            onClick={() =>
+              setMovies(
+                movies.map((movie) =>
+                  movie.id === m.id
+                    ? { ...movie, title: "Updated Title" }
+                    : movie,
+                ),
+              )
+            }>
+            Update Title
+          </button>
+          <button
+            onClick={() =>
+              setMovies(movies.filter((movie) => movie.id !== m.id))
+            }>
+            Delete Movie
+          </button>
+          <button
+            onClick={() =>
+              setMovies([
+                ...movies,
+                {
+                  id: movies.length + 1,
+                  title: "New Movie",
+                  releaseYear: 2020,
+                },
+              ])
+            }>
+            Add Movie
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
